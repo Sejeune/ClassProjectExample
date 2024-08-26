3289d265753e9539b4ae14f5fea6148d6bcb613f
Sejeune <philemonsejeune@gmail.com>
2024-08-26 09:04:10 +0100

    Our first commit for the project

---
+++ src/Utils/Constants.java
@@ -0,0 +1,16 @@
+package Utils;
+
+public class Constants {
+	
+	public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir")
+                     + "/configs/configuration.properties";
+
+
+public static final int IMPLICIT_WAIT_TIME = 10;
+
+public static final int EXPLICIT_WAIT_TIME = 30;
+
+
+
+
+}
